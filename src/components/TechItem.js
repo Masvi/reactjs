c2346c4c8afecc954da4c2597eee79b15e245749
masvi <jeanmasvi@gmail.com>
2020-03-17 08:27:04 -0300

    prop types

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TechItem({ tech, onDelete }) {
     return (<li>
         {tech}
         <button onClick={onDelete} type="button">delete</button>
     </li>);
+}
+
+TechItem.defaultProps = {
+    tech: 'Oculto',
+}
+
+//validacao de props
+TechItem.propTypes = {
+    tech: PropTypes.string,
+    onDelete: PropTypes.func.isRequired,
 }
 
 export default TechItem;
